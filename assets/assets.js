0cd9da1728118876459a7fcefd52c0c79f89d24e
Waseeq <rinky.zafar@gmail.com>
2025-06-27 18:07:54 +0530

    fix

--- assets/assets.js
+++ assets/assets.js
@@ -119,7 +119,7 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Java, Python, c++'},
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'Java, Python, C++'},
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science and Engineering' },
       {
     icon: assets.project_icon,
